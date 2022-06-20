9390082d89cdefb8350e16a76cbfe238f060b972
beshahadat <shahadatnayeem23@gmail.com>
2022-06-21 02:26:46 +0600

    basic infrastructure

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="/ideable/css/style.css">
+    <script src="https://kit.fontawesome.com/05ce598124.js" crossorigin="anonymous"></script>
+
+    <title>ideable</title>
+    <link rel="icon" href="media/ideable_logo.png">
+</head>
+<body>
+    
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include "includes/header.php";
+include "includes/navbar.php";
+include "includes/dbConfig.php";
+?>
+
+<?php
+include "includes/footer.php";
+?>
