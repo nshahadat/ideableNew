1d88aea956407a0ef3c0a41189598c721b2a23b8
beshahadat <shahadatnayeem23@gmail.com>
2022-06-22 01:53:19 +0600

    editing index page

--- index.php
+++ index.php
@@ -3,12 +3,11 @@ include "includes/header.php";
 include "includes/navbar.php";
 include "includes/dbConfig.php";
 ?>
-
-<section>
-    <div class="main-banner">
-        <img src="/ideable/media/handshake.jpg" class="main-banner-img">
+    <div class="container">
+        <div class="banner-text-1">
+            <p>An idea that is developed and put into action is more important than an idea that exists only as an idea</p>
+        </div>
     </div>
-</section>
 
 <?php
 include "includes/footer.php";
