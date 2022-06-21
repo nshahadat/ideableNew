dd13aa60ebbc80062c6238e6bd1f2c004818fb2d
beshahadat <shahadatnayeem23@gmail.com>
2022-06-21 20:24:17 +0600

    navbar completed

---
+++ includes/navbar.php
@@ -0,0 +1,29 @@
+<nav class="small-navbar">
+    <ul>
+        <li><a href="#">How <strong>ideable</strong> works</a></li>
+        <li><a href="#">Our collaboration partners</a></li>
+        <li><a href="#">Term and Policies</a></li>
+    </ul>
+    <section id="left-ul">
+        <li><a href="#" id="left-child">About <strong>ideable</strong> |</a></li>
+    </section>
+</nav>
+<section id="logo-section">
+    <img src="/ideable/media/ideable_logo.png" class="main-logo">
+</section>
+<nav class="main-navbar">
+    <ul>
+        <li><a href="#">investor</a></li>
+        <div class="dropdown">
+            <li><a href="#"><button>Categories</button></a></li>
+            <div class="dropdown-content">
+                <a href="#">Technology</a>
+                <a href="#">Farming</a>
+                <a href="#">Media</a>
+                <a href="#">Education</a>
+            </div>
+        </div>
+        <li><a href="#">Expertise</a></li>
+        <li><a href="#">inventor</a></li>
+        <li><a href="#">Search</a></li>
+</nav>
--- index.php
+++ index.php
@@ -4,6 +4,12 @@ include "includes/navbar.php";
 include "includes/dbConfig.php";
 ?>
 
+<section>
+    <div class="main-banner">
+        <img src="/ideable/media/handshake.jpg" class="main-banner-img">
+    </div>
+</section>
+
 <?php
 include "includes/footer.php";
 ?>
