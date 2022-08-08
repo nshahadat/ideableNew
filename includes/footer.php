71ff25dcee0ea0cd63a3acd88eec6d76a40a6bc7
Arnob <arnobm97@gmail.com>
2022-08-08 18:15:33 +0600

    footer added

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,39 @@
-
-
-
-    <script type="text/javascript" src="/ideable/js/script.js"></script>
-    <script type="text/javascript" src="/ideable/js/app.js"></script>
+<script type="text/javascript" src="/ideable/js/script.js"></script>
+<script type="text/javascript" src="/ideable/js/app.js"></script>
 </body>
+<footer>
+    <div class="footrow">
+        <div class="footcol">
+            <img src="/ideable/media/ideable_logo.png" class="logo">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.517159788671!2d90.41937981530928!3d23.72893068459985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b85adfae47d1%3A0xe923011c649c5aa7!2sNotre%20Dame%20University%20Bangladesh!5e0!3m2!1sen!2sbd!4v1659879094590!5m2!1sen!2sbd" 
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="footcol">
+            <h3>Office <div class="underline"></div><span></span></h3>
+            <ul>
+            <li>2/A, Arambagh, Motijheel, Dhaka 1000</li>
+            <li class="foot_mail">ideabletest@gmail.com</li>
+            </ul>
+            <h4>+88001874455575</h4>
+        </div>
+        <div class="footcol"><h3>Links <div class="underline"><span></span></h3>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Terms and Policies</a></li>
+        </ul>
+        </div>
+        <div class="footcol"><div class="social_icons">
+            <h3>social links <div class="underline"><span></span></div></h3>
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands  fa-whatsapp"></i>
+        <i class="fa-brands  fa-pinterest"></i>
+        
+        </div><p class="copyright">  &copy; Shahadat Nayeem | Hasibul Hassan | Arnob Mallick</p></div>
+        
+    </div>
+    
+    
+</footer>
 </html>
