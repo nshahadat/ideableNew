ff841015599a0bd6fb42b9e769bbe8ae0316a6bd
Arnob <arnobm97@gmail.com>
2022-09-20 21:21:47 +0600

    about us page added

---
+++ about-us.php
@@ -0,0 +1,36 @@
+<?php
+include "includes/header.php";
+include "includes/navbar-main.php";
+include "includes/dbConfig.php";
+?>
+<div class="wrapper1">
+  <h1 class="aboutheader">Our Team</h1>
+  <div class="team">
+    <div class="team_member">
+      <div class="team_img">
+        <img src="/ideable/media/shahadat.jpg" alt="Team_image">
+      </div>
+      <h3>Shahadat Nayeem</h3>
+      <p class="role">Backend Developer</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
+    </div>
+    <div class="team_member">
+      <div class="team_img">
+        <img src="/ideable/media/safran.jpg" alt="Team_image">
+      </div>
+      <h3>Hasibul Hassan</h3>
+      <p class="role">Frontend Developer</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p></div>
+    <div class="team_member">
+      <div class="team_img">
+        <img src="/ideable/media/arnob.jpg" alt="Team_image">
+      </div>
+      <h3>Arnob Mallick</h3>
+      <p class="role">Frontend Developer</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
+    </div>
+  </div>
+</div>	
+<?php
+include "includes/footer.php";
+?>
