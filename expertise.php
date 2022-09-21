7a024e40faf47de77149bc671c5d5291c902e9fe
Arnob <arnobm97@gmail.com>
2022-09-21 23:38:55 +0600

    expertise page added

--- expertise.php
+++ expertise.php
@@ -3,17 +3,50 @@ include "includes/header.php";
 include "includes/navbar-main.php";
 include "includes/dbConfig.php";
 ?>
-<h1 class="ex-header">Somthing</h1>
+<h1 class="ex-header1">Something</h1>
+<h1 class="ex-header2">Something</h1>
 <div class="gallery1">
             <div class="card1">
             <div class="card1-image"><img src="" alt=""></div>
             <div class="card1-text">
-                <span class="date">4 days ago</span>
                 <h2>Post One</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
             </div>
-            
             </div>
+            <div class="card1">
+            <div class="card1-image card12"></div>
+            <div class="card1-text card12">
+                <h2>Post Two</h2>
+                <p>Adipisicing elit. Ducimus, repudiandae corrupti amet temporibus omnis provident illum maxime quod. Lorem ipsum dolor</p>
+            </div>
+            </div>
+           <hr class="hrline">
+            <div class="card1">
+                <div class="card1-image card13"></div>
+                <div class="card1-text card13">
+                <h2>Post Three</h2>
+                <p>Repudiandae corrupti amet temporibus omnis provident illum maxime. Ducimus, lorem ipsum dolor adipisicing elit</p>
+                </div>
+            </div>
+            <div class="card1">
+            <div class="card1-image"></div>
+            <div class="card1-text">
+                <h2>Post One</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
+            </div>
+            </div>
+    </div>
+    <h1 class="ex-header1">Something</h1>
+    <h1 class="ex-header2">Something</h1>
+<div class="gallery1">
+            <div class="card1">
+            <div class="card1-image"><img src="" alt=""></div>
+            <div class="card1-text">
+                <h2>Post One</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
+            </div>
+            </div>
+            <hr class="hrline">
             <div class="card1">
             <div class="card1-image card12"></div>
             <div class="card1-text card12">
@@ -31,108 +64,12 @@ include "includes/dbConfig.php";
             <div class="card1">
             <div class="card1-image"></div>
             <div class="card1-text">
-                <span class="date">4 days ago</span>
                 <h2>Post One</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
             </div>
             </div>
     </div>
-    <h1 class="ex-header">Something</h1>
-    <div class="gallery1">
-            <div class="card1">
-            <div class="card1-image"></div>
-            <div class="card1-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            </div>
-            <div class="card1">
-            <div class="card1-image card12"></div>
-            <div class="card1-text card12">
-                <span class="date">1 week ago</span>
-                <h2>Post Two</h2>
-                <p>Adipisicing elit. Ducimus, repudiandae corrupti amet temporibus omnis provident illum maxime quod. Lorem ipsum dolor</p>
-            </div>
-            </div>
-            <div class="card1">
-                <div class="card1-image card13"></div>
-                <div class="card1-text card13">
-                <h2>Post Three</h2>
-                <p>Repudiandae corrupti amet temporibus omnis provident illum maxime. Ducimus, lorem ipsum dolor adipisicing elit</p>
-                </div>
-            </div>
-            <div class="card1">
-            <div class="card1-image"></div>
-            <div class="card1-text">
-                  <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-        </div>
-    </div>
-    <h1 class="ex-header">Something</h1>
-    <div class="gallery1">
-            <div class="card1">
-            <div class="card1-image"></div>
-            <div class="card1-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            </div>
-            <div class="card1">
-            <div class="card1-image card12"></div>
-            <div class="card1-text card12">
-                <span class="date">1 week ago</span>
-                <h2>Post Two</h2>
-                <p>Adipisicing elit. Ducimus, repudiandae corrupti amet temporibus omnis provident illum maxime quod. Lorem ipsum dolor</p>
-            </div>
-            </div>
-            <div class="card1">
-                <div class="card1-image card13"></div>
-                <div class="card1-text card13">
-                <h2>Post Three</h2>
-                <p>Repudiandae corrupti amet temporibus omnis provident illum maxime. Ducimus, lorem ipsum dolor adipisicing elit</p>
-                </div>
-            </div>
-            <div class="card1">
-            <div class="card1-image"></div>
-            <div class="card1-text">
-                  <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-        </div>
-    </div>
-    <h1 class="ex-header">Something</h1>
-    <div class="gallery1">
-            <div class="card1">
-            <div class="card1-image"></div>
-            <div class="card1-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            </div>
-            <div class="card1">
-            <div class="card1-image card12"></div>
-            <div class="card1-text card12">
-                <span class="date">1 week ago</span>
-                <h2>Post Two</h2>
-                <p>Adipisicing elit. Ducimus, repudiandae corrupti amet temporibus omnis provident illum maxime quod. Lorem ipsum dolor</p>
-            </div>
-            </div>
-            <div class="card1">
-                <div class="card1-image card13"></div>
-                <div class="card1-text card13">
-                <h2>Post Three</h2>
-                <p>Repudiandae corrupti amet temporibus omnis provident illum maxime. Ducimus, lorem ipsum dolor adipisicing elit</p>
-                </div>
-            </div>
-            <div class="card1">
-            <div class="card1-image"></div>
-            <div class="card1-text">
-                  <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-        </div>
-    </div>
-    <?php
+   
+<?php
 include "includes/footer2.php";
 ?>
