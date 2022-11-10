d59cd41c1d6605ac8afa6be79dad41cb9618779f
Arnob <arnobm97@gmail.com>
2022-11-10 17:08:58 +0600

    profiles added

---
+++ investor/investor-profile.php
@@ -0,0 +1,21 @@
+<?php
+define('ADMIN', 'C:/xampp/htdocs/ideable');
+include ADMIN . '/includes/header.php';
+include ADMIN . '/includes/navbar-main.php';
+include ADMIN . '/includes/dbConfig.php';
+?>
+<div class="investor-pro-container">
+    <h3 class="inventor-prof-header">Your Information</h3>
+    <button class="inventor-prof-btn1">edit your information</button>
+    <p class="inventor-profile"><span>Name:</span> Arnob Mallick</p>
+    <p class="inventor-profile"><span>Email:</span> Arnob Mallick</p>
+    <p class="inventor-profile"><span>About:</span> Arnob Mallick</p>
+    <p class="inventor-profile"><span>Password:</span> Arnob Mallick</p>
+    <h3 class="inventor-prof-header">Current You're Interested In</h3>
+    <p class="inventor-profile"><span>1.</span> Idea1</p>
+    <p class="inventor-profile"><span>2.</span> Idea2</p>
+    <p class="inventor-profile"><span>3.</span> Idea3</p>
+</div>
+<?php
+include ADMIN . '/includes/footer.php';
+?> 
