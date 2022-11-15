dcc7f5a1e27e23fdca6e3452ac53d257bd5d4487
Arnob <arnobm97@gmail.com>
2022-11-15 22:04:48 +0600

    ideas added

--- categoreis/education.php
+++ categoreis/education.php
@@ -3,60 +3,20 @@ define('ADMIN', 'C:/xampp/htdocs/ideable');
 include ADMIN . '/includes/header.php';
 include ADMIN . '/includes/navbar-main.php';
 include ADMIN . '/includes/dbConfig.php';
-error_reporting(0);
-session_start();
-
-// if(!isset($_SESSION['email'])){
-//     echo "<script>alert('You have to login first');
-//     window.location='/ideable/inventor/inventor-login.php';</script>";
-//     die();
-// }
-
-$user_email = $_SESSION['email'];
-$user_find_sql = "SELECT * FROM $investor WHERE investor_email = '$user_email'";
-$result  = mysqli_query($mysqli, $user_find_sql) or die(mysqli_error($mysqli));
-$data = mysqli_fetch_array($result);
-$username = $data['investor_name'];
-
-// backend of post management and show 
-
-$post_fetch_sql = "SELECT * FROM $post";
-$post_fetch_result  = $mysqli->query($post_fetch_sql);
-while ($post_fetch_data = $post_fetch_result->fetch_assoc()) {
-    $post_fetch_username = $post_fetch_data['inventor_name'];
-    $post_fetch_title = $post_fetch_data['title'];
-    $post_fetch_des = $post_fetch_data['description'];
-    $post_fetch_tp = $post_fetch_data['theoratically_proven'];
-    $post_fetch_pp = $post_fetch_data['practically_proven'];
-    $post_fetch_demo = $post_fetch_data['demo'];
-    $post_fetch_bm = $post_fetch_data['business_model'];
-    $post_fetch_patent = $post_fetch_data['patent'];
-    $post_fetch_at = $post_fetch_data['approximate_time'];
-    $post_fetch_inventor_id = $post_fetch_data['inventor_id'];
-    $post_fetch_inventor_email = $post_fetch_data['inventor_email'];    
-    $_SESSION['post_fetch_thumb_dir'] = $post_fetch_data['thumb_files_dir'];
-    $_SESSION['post_fetch_doc_dir'] = $post_fetch_data['doc_files_dir'];
-    $_SESSION['post_fetch_media_dir'] = $post_fetch_data['media_files_dir'];
-    $_SESSION['post_fetch_speech_dir'] = $post_fetch_data['speech_files_dir'];
-    ?>
-
-<div class="username-btn"><button><?php echo $username; ?></button></div>
-<a href="investor-logout.php"><div class="logout-btn"><button>logout</button></div></a>
-<form action="#" method="post">
+?>
 <div class="feed-container">
 <section class="feed-left">
-    <h1 class="userheader"> <?php echo $post_fetch_username; ?> has an idea of <?php echo $post_fetch_title; ?></h1>
-    <input type="hidden" name="get_inventor_id" value="<?php echo $post_fetch_inventor_id ?>">
-    <input type="hidden" name="get_inventor_email" value="<?php echo $post_fetch_inventor_email ?>">
+    <h1 class="userheader">  has an idea of </h1>
+    <input type="hidden" name="get_inventor_id" value="">
+    <input type="hidden" name="get_inventor_email" value="">
     <button class="feed-btn1">like</button>
     <button class="feed-btn2">report</button>
-    <img src="<?php echo $post_fetch_data['thumb_files_dir']; ?>" class="feed-img " alt="">
+    <img src="" class="feed-img " alt="">
     <h1 style="color: #2E5261">
-    <?php echo $post_fetch_title; ?>
     </h1>
 
     <p>
-    <?php echo $post_fetch_des; ?>
+    
         <span id="points">...</span>
 
         <!-- Define the text that would be 
@@ -140,7 +100,7 @@ while ($post_fetch_data = $post_fetch_result->fetch_assoc()) {
     </script>
     <a href="/ideable/investor/idea-required-files.php" target="_blank">Media Files</a>
     <a href="/ideable/investor/idea-required-files.php" target="_blank">Doc Files</a>
-    <p >Theoratically Proved: <?php echo $post_fetch_tp; ?>; Practically Proved: <?php echo $post_fetch_pp; ?>; Demo: <?php echo $post_fetch_demo; ?>; Business Model: <?php echo $post_fetch_bm; ?>; Patent: <?php echo $post_fetch_patent; ?>; Approximate Time: <?php echo $post_fetch_at; ?></p>
+   Theoratically Proved:  Practically Proved:  Demo: Business Model: Patent: Approximate Time: </p>
     <h6 class ="ques">ask any questions before investing (if any)</h6>
     <textarea spellcheck="false" name="toi" placeholder="Type something here..."></textarea>
                 <script>
@@ -148,29 +108,18 @@ while ($post_fetch_data = $post_fetch_result->fetch_assoc()) {
                 textarea.addEventListener("keyup", e =>{
                     textarea.style.height = "63px";
                     let scHeight = e.target.scrollHeight;
-                    textarea.style.height = `${scHeight}px`;
+                    textarea.style.height = ${scHeight}px;
                 });
                 </script> 
     <button class="ask-btn" name = "ask-btn">ask</button></form>
     <p>or click this button if you are interested in investing</p>
     <form action="/ideable/investor/investor-feed-confirm-email.php" method="post">
         <button class="invest-btn" name="invest-confirm-btn">invest</button>
-        <input type="hidden" name="get_inventor_email" value="<?php echo $post_fetch_inventor_email ?>">
+        <input type="hidden" name="get_inventor_email" value="">
     </form>
     <p class="terms-policies">before investing read our <a href="#">terms and policies</a></p>
     </div>
 </section>
-
-<?php
-    } 
-
-    if (isset($_POST['invest-confirm-btn'])) {
-        $_SESSION['send_inventor_email'] = $post_fetch_inventor_email;
-        $_SESSION['send_inventor_idea_title'] = $post_fetch_title;
-    }
-
-?>
-
 <?php
 include ADMIN . '/includes/footer.php';
 ?>
