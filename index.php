cf0ad416538eb3c0a621bab284d5f98896f538a9
Arnob <arnobm97@gmail.com>
2022-12-01 20:25:07 +0600

    cards added

--- index.php
+++ index.php
@@ -3,213 +3,62 @@ include "includes/header.php";
 include "includes/navbar.php";
 include "includes/dbConfig.php";
 ?>
-        <div class="introduction">
-            <video id="indexVid" width="500" controls><source src="media/index-introduction.mp4" type="video/mp4"></video>
-        </div>
-        <div class="banner-text-1">
-            <p>An idea that is developed and put into action is more important than an idea that exists only as an idea</p>
-        </div>
-        <div class="banner-info">
-            <p class="banner-info-text">Why you should join ideable</p>
-            <div class="info">
-                <div class="info-part"><img src="/ideable/media/inventor_index.png" height="300px" width="300px"></div>
-                <div class="info-part">
-                    <h1 style="font-weight: 300;">inventor?</h1>
-                    <h3 style="font-weight: 200; text-align:justify; padding-right:5vw;">ideable provides you hassle-free process for getting investments. Though you need to remember that this is not crowdfunding or charity rising, you're going to a one-to-one business with every investor who is investing on you.</h3>
-                </div>
-            </div>
-        <div class="info">
-            <div class="info-part">
-                <h1 style="font-weight: 300;">investor?</h1>
-                <h3 style="font-weight: 200; text-align:justify; padding-left:5vw;">ideable is the one place where you can directly contact with the person you're investing and even you can ask for an expert to do the job for you. Either way you are having a full knowledge of the project you are investing on.</h3>
-            </div>
-            <div class="info-part"><img src="/ideable/media/investor_index.png" height="300px" width="300px"alt=""></div>
-        </div>
-        <div class="banner-text-1">
-            <p style="font-family: 'Inter', sans-serif;font-size: 2vw;">Make your idea able to change the world</p>
-        </div>
-        <div class="info">
-            <div class="info-part"  style="padding-top: 300px;">
-                <p class="banner-info-text">New to investment? Check this out</p>
-                <video width="500" controls controlslist="nodownload"><source src="media/investor.mp4" type="video/mp4"></video>
-            </div>
-        </div>
-        <div class="banner-text-1">
-            <p style="font-family: 'Inter', sans-serif;font-size: 2vw;">Even the simplest idea can give you the greatest success</p>
-        </div>
-        <div class="info-part" style="padding-bottom: 40px; font-size:3vw; width:100vw !important; padding-top:300px;">ideas that got investment</div>
-        <div class="gallery">
-            <div class="card">
-            <div class="card-image"></div>
-            <div class="card-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            <div class="card-stats">
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-                <div class="stat border">
-                <div class="value">Technology</div>
-                <div class="type"></div>
-                </div>
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-            </div>
-            </div>
-            <div class="card">
-            <div class="card-image"></div>
-            <div class="card-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            <div class="card-stats">
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-                <div class="stat border">
-                <div class="value">Technology</div>
-                <div class="type"></div>
-                </div>
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-            </div>
-            </div>
-            <div class="card">
-            <div class="card-image"></div>
-            <div class="card-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            <div class="card-stats">
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-                <div class="stat border">
-                <div class="value">Technology</div>
-                <div class="type"></div>
-                </div>
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-            </div>
-            </div>
-            <div class="card">
-            <div class="card-image"></div>
-            <div class="card-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            <div class="card-stats">
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-                <div class="stat border">
-                <div class="value">Technology</div>
-                <div class="type"></div>
-                </div>
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-            </div>
-        </div>
+ <div class="introduction">
+    <video id="indexVid" width="500" controls><source src="media/index-introduction.mp4" type="video/mp4"></video>
+</div>
+ <div class="banner-text-1">
+    <p>An idea that is developed and put into action is more important than an idea that exists only as an idea</p>
+</div>
+<div class="banner-info">
+     <p class="banner-info-text">Why you should join ideable</p>
+<div class="info">
+    <div class="info-part"><img src="/ideable/media/inventor_index.png" height="300px" width="300px"></div>
+    <div class="info-part">
+    <h1 style="font-weight: 300;">inventor?</h1>
+    <h3 style="font-weight: 200; text-align:justify; padding-right:5vw;">ideable provides you hassle-free process for getting investments. Though you need to remember that this is not crowdfunding or charity rising, you're going to a one-to-one business with every investor who is investing on you.</h3>
     </div>
-    <div class="gallery">
-            <div class="card">
-            <div class="card-image"></div>
-            <div class="card-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            <div class="card-stats">
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-                <div class="stat border">
-                <div class="value">Technology</div>
-                <div class="type"></div>
-                </div>
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-            </div>
-            </div>
-            <div class="card">
-            <div class="card-image"></div>
-            <div class="card-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            <div class="card-stats">
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-                <div class="stat border">
-                <div class="value">Technology</div>
-                <div class="type"></div>
-                </div>
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-            </div>
-            </div>
-            <div class="card">
-            <div class="card-image"></div>
-            <div class="card-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            <div class="card-stats">
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-                <div class="stat border">
-                <div class="value">Technology</div>
-                <div class="type"></div>
-                </div>
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-            </div>
-            </div>
-            <div class="card">
-            <div class="card-image"></div>
-            <div class="card-text">
-                <h2>Post One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
-            </div>
-            <div class="card-stats">
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-                <div class="stat border">
-                <div class="value">Technology</div>
-                <div class="type"></div>
-                </div>
-                <div class="stat">
-                <div class="value"></div>
-                <div class="type"></div>
-                </div>
-            </div>
-        </div>
+</div>
+<div class="info">
+    <div class="info-part">
+    <h1 style="font-weight: 300;">investor?</h1>
+    <h3 style="font-weight: 200; text-align:justify; padding-left:5vw;">ideable is the one place where you can directly contact with the person you're investing and even you can ask for an expert to do the job for you. Either way you are having a full knowledge of the project you are investing on.</h3>
     </div>
+<div class="info-part"><img src="/ideable/media/investor_index.png" height="300px" width="300px"alt=""></div>
+</div>
+ <div class="banner-text-1">
+    <p style="font-family: 'Inter', sans-serif;font-size: 2vw;">Make your idea able to change the world</p>
+</div>
+<div class="info">
+    <div class="info-part"  style="padding-top: 300px;">
+     <p class="banner-info-text">New to investment? Check this out</p>
+            <video width="500" controls controlslist="nodownload"><source src="media/investor.mp4" type="video/mp4"></video>
+     </div>
+</div>
+<div class="banner-text-1">
+    <p style="font-family: 'Inter', sans-serif;font-size: 2vw;">Even the simplest idea can give you the greatest success</p>
+</div>
+    <div class="info-part" style="padding-bottom: 40px; font-size:3vw; width:100vw !important; padding-top:300px;">ideas that got investment</div>
+<h1>Card Image with description on :hover</h1>
+<div class="gal-container">
+            <div class="gallerys">
+            <div class="gallery">
+                <h2 class="gallery-title">Seal</h2>
+                <img src="https://images.unsplash.com/photo-1591485423007-765bde4139ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt="">
+                <p class="gallery-desc">Pinnipeds, commonly known as seals,[a] are a widely distributed and diverse clade of carnivorous, fin-footed, semiaquatic marine mammals. They comprise the extant families Odobenidae (whose only living member is the walrus), Otariidae (the eared seals: sea lions and fur seals), and Phocidae (the earless seals, or true seals).</p>
+            </div>
+            <div class="gallery">
+                <h2 class="gallery-title">Lion</h2>
+                <img src="https://images.unsplash.com/photo-1534188753412-3e26d0d618d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="">
+                <p class="gallery-desc">The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions have a prominent mane.</p>
+            </div>
+            <div class="gallery">
+                <h2 class="gallery-title">Hawk</h2>
+                <img src="https://images.unsplash.com/photo-1534251369789-5067c8b8602a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="">
+                <p class="gallery-desc">Hawks are a group of medium-sized diurnal birds of prey of the family Accipitridae. Hawks are widely distributed and vary greatly in size.
+                The subfamily Accipitrinae includes goshawks, sparrowhawks, sharp-shinned hawks and others. This subfamily are mainly woodland birds with long tails and high visual acuity. They hunt by dashing suddenly from a concealed perch.</p>
+    </div>
+</div>
+</div>          
     
 
 <?php
