f062837dc797a3f3e475a3b59095e637f2fced3a
beshahadat <shahadatnayeem23@gmail.com>
2023-01-07 16:59:35 +0600

    minor

--- investor/investor-feed-confirm-email.php
+++ investor/investor-feed-confirm-email.php
@@ -12,40 +12,41 @@ use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
 
 
-function send_confirm_email($user_email){
+function send_confirm_email($user_email)
+{
     $mail = new PHPMailer(true);
 
-try {
-    //Server settings
+    try {
+        //Server settings
 
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'ideabletest@gmail.com';                     //SMTP username
-    $mail->Password   = 'cfblztamjiasttix';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->isSMTP(); //Send using SMTP
+        $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
+        $mail->SMTPAuth = true; //Enable SMTP authentication
+        $mail->Username = 'ideabletest@gmail.com'; //SMTP username
+        $mail->Password = 'cfblztamjiasttix'; //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+        $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('ideabletest@gmail.com', 'ideable');
-    $mail->addAddress($_SESSION['send_inventor_email']);     //Add a recipient
+        //Recipients
+        $mail->setFrom('ideabletest@gmail.com', 'ideable');
+        $mail->addAddress($_SESSION['send_inventor_email']); //Add a recipient
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'New investor on your idea';
-    $mail->Body    = "Hello Sir, your idea on ideable got a new interest who is $user_email";
+        //Content
+        $mail->isHTML(true); //Set email format to HTML
+        $mail->Subject = 'New investor on your idea';
+        $mail->Body = "Hello Sir, your idea on ideable got a new interest who is $user_email";
 
-    $mail->send();
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
-}
 
-    $user_email = $_SESSION['email'];
+$user_email = $_SESSION['email'];
 
-    send_confirm_email($user_email);
+send_confirm_email($user_email);
 
-    echo "<script>alert('An email has been sent to the investor. Please wait until he contacts you.');
+echo "<script>alert('An email has been sent to the inventor. Please wait until he contacts you.');
     window.location='/ideable/investor/investor-feed.php';</script>";
 
 ?>
