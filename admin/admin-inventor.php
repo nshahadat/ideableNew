1a7b88c5ac0ff9051eee5f13cac520486721d647
Arnob <arnobm97@gmail.com>
2023-01-16 05:02:34 +0600

    asdasd

--- admin/admin-inventor.php
+++ admin/admin-inventor.php
@@ -4,6 +4,10 @@ include ADMIN . '/includes/header.php';
 include ADMIN . '/includes/navbar-main.php';
 include ADMIN . '/includes/dbConfig.php';
 ?>
+<div class="username-btn"><button><?php echo $username; ?></button></div>
+        <a href="investor-logout.php">
+            <div class="logout-btn"><button>logout</button></div>
+        </a>
 <div class="table-container">
         <hr>
         <input type="text" id="myInput" onkeyup='tableSearch()' placeholder="Name">
