0c5b171746596234aa8b955150d8d5ceb0d6e398
Arnob <arnobm97@gmail.com>
2023-01-16 22:40:13 +0600

    asdasd

--- collaboration.php
+++ collaboration.php
@@ -9,61 +9,46 @@ include ADMIN . '/includes/dbConfig.php';
  <ul class="cards">
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/micc.png" class="card__image1" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
           <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>            
+            <h3 class="card__title">Microsoft</h3>            
             <!-- <span class="card__status">1 hour ago</span> -->
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Microsoft Corporation is an American multinational technology corporation producing computer software  and related services.This company helps us to review ideas that are in technology category</p>
       </div>
     </a>      
   </li>
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/g.png" class="card__image" alt="" />
       <div class="card__overlay">        
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
+            <h3 class="card__title">Google</h3>
             <!-- <span class="card__status">3 hours ago</span> -->
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">The company has since rapidly grown to offer a multitude of products and services beyond Google Search, many of which hold dominant market positions.This company helps us to review ideas that are in technology category</p>
       </div>
     </a>
   </li>
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>      
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/intt.png" class="card__image" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
+            <h3 class="card__title">Intel</h3>
             <!-- <span class="card__status">3 hours ago</span> -->
           </div>          
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">intel Corporation is an American multinational corporation .This company helps us to review ideas that are in technology category </p>
       </div>
     </a>
   </li>    
@@ -72,61 +57,46 @@ include ADMIN . '/includes/dbConfig.php';
 <ul class="cards">
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/joh.png" class="card__image" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
           <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>            
+            <h3 class="card__title">John Deere</h3>            
             <!-- <span class="card__status">1 hour ago</span> -->
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Deere & Company is listed on the New York Stock Exchange under the symbol DE.This company helps us to review ideas that are in Farming category.</p>
       </div>
     </a>      
   </li>
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">        
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/adm.jpg" class="card__image" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
           <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>      
+            <h3 class="card__title">ADM</h3>      
             <!-- <span class="card__status">1 hour ago</span> -->
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">ADM ranked No. 54 in the 2020 Fortune 500 list of the largest United States corporations.This company helps us to review ideas that are in Farming category</p>
       </div>
     </a>
   </li>
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/syy.png" class="card__image" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
+            <h3 class="card__title">Syngenta</h3>
             <!-- <span class="card__status">3 hours ago</span> -->
           </div>          
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Syngenta AG is a provider of agricultural science and Farming, in particular seeds and pesticides with its management headquarters in Basel, Switzerland.This company helps us to review ideas that are in Farming category</p>
       </div>
     </a>
   </li>    
@@ -135,61 +105,46 @@ include ADMIN . '/includes/dbConfig.php';
 <ul class="cards">
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/marvv.png" class="card__image4" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
           <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>            
+            <h3 class="card__title">Marvel</h3>            
             <!-- <span class="card__status">1 hour ago</span> -->
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Marvel Studios releases its films in groups called "Phases", with the first three phases collectively known as "The Infinity Saga" and the following three phases as "The Multiverse Saga". </p>
       </div>
     </a>      
   </li>
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/dc.jpg" class="card__image" alt="" />
       <div class="card__overlay">        
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
+            <h3 class="card__title">DC</h3>
             <!-- <span class="card__status">3 hours ago</span> -->
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">DC Comics is one of the largest and oldest American comic book companies.This company helps us to review ideas that are in Media category</p>
       </div>
     </a>
   </li>
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>      
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/sony.jpg" class="card__image" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
+            <h3 class="card__title">Sony Music</h3>
             <!-- <span class="card__status">3 hours ago</span> -->
           </div>          
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Sony Music Entertainment (SME), simply known as Sony Music, is an American multinational music company owned by Sony Entertainment</p>
       </div>
     </a>
   </li>    
@@ -198,61 +153,46 @@ include ADMIN . '/includes/dbConfig.php';
 <ul class="cards">
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/haarv.png" class="card__image" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
           <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>            
+            <h3 class="card__title">Harvard University</h3>            
             <!-- <span class="card__status">1 hour ago</span> -->
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Harvard University is a private Ivy League research university in Cambridge, Massachusetts. </p>
       </div>
     </a>      
   </li>
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/ox.png" class="card__image6" alt="" />
       <div class="card__overlay">        
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
+            <h3 class="card__title">Oxford</h3>
             <!-- <span class="card__status">3 hours ago</span> -->
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">The University of Oxford is a collegiate research university in Oxford, England.</p>
       </div>
     </a>
   </li>
   <li>
     <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>      
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+      <img src="/ideable/media/ndub.jpg" class="card__image8" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
+            <h3 class="card__title">Notre Dame University Bangladesh </h3>
             <!-- <span class="card__status">3 hours ago</span> -->
           </div>          
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Notre Dame University Bangladesh or NDUB  is a private Catholic research university in Bangladesh. </p>
       </div>
     </a>
   </li>    
