d5d6d686ed06374004ca7d265726d95862e3a3c0
Arnob <arnobm97@gmail.com>
2023-03-10 00:10:33 +0600

    ask btn fixed

--- collaboration.php
+++ collaboration.php
@@ -1,268 +1,239 @@
-<?php
+?php
 define('ADMIN', 'C:/xampp/htdocs/ideable');
 include ADMIN . '/includes/header.php';
 include ADMIN . '/includes/navbar-main.php';
 include ADMIN . '/includes/dbConfig.php';
 ?>
 <div class="card-container">
-<div class="card-header">Technology</div>
- <ul class="cards">
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>            
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>      
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">        
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
+  <div class="card-header">Technology</div>
+  <ul class="cards">
+
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/micc.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">Microsoft</h3>
+              <!-- <span class="card__status">3 hours ago</span> -->
+            </div>
           </div>
           <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
           </p>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>      
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
-          </div>          
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>    
-</ul>
-<div class="card-header">Farming</div>
-<ul class="cards">
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>            
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>      
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">        
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>      
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
-          </div>          
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>    
-</ul>
-<div class="card-header">Media</div>
-<ul class="cards">
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>            
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>      
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">        
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
+      </a>
+    </li>
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/g.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">Google</h3>
+              <!-- <span class="card__status">1 hour ago</span> -->
+            </div>
           </div>
           <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
           </p>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>      
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
-          </div>          
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>    
-</ul>
-<div class="card-header">Education</div>
-<ul class="cards">
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>            
-            <!-- <span class="card__status">1 hour ago</span> -->
-          </div>
-        </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>      
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">        
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
+      </a>
+    </li>
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/hhh.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">Huawei</h3>
+              <!-- <span class="card__status">3 hours ago</span> -->
+            </div>
           </div>
           <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
           </p>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Jessica Parker</h3>      
-            <!-- <span class="card__status">1 hour ago</span> -->
+      </a>
+    </li>
+  </ul>
+  <div class="card-header">Farming</div>
+  <ul class="cards">
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/joh.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">John Deere</h3>
+              <!-- <span class="card__status">3 hours ago</span> -->
+            </div>
           </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="" class="card">
-      <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">kim Cattrall</h3>
-            <!-- <span class="card__status">3 hours ago</span> -->
-          </div>          
+      </a>
+    </li>
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/fmc.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">FMC</h3>
+              <!-- <span class="card__status">1 hour ago</span> -->
+            </div>
+          </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-      </div>
-    </a>
-  </li>    
-</ul>
+      </a>
+    </li>
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/adm.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">ADM</h3>
+              <!-- <span class="card__status">3 hours ago</span> -->
+            </div>
+          </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
+        </div>
+      </a>
+    </li>
+  </ul>
+  <div class="card-header">Media</div>
+  <ul class="cards">
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/marvv.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">Marvel</h3>
+              <!-- <span class="card__status">3 hours ago</span> -->
+            </div>
+          </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/dc.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">DC</h3>
+              <!-- <span class="card__status">1 hour ago</span> -->
+            </div>
+          </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/tss.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">T Series</h3>
+              <!-- <span class="card__status">3 hours ago</span> -->
+            </div>
+          </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
+        </div>
+      </a>
+    </li>
+  </ul>
+  <div class="card-header">Education</div>
+  <ul class="cards">
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/haarv.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">Harvard University</h3>
+              <!-- <span class="card__status">3 hours ago</span> -->
+            </div>
+          </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/ox.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">Oxford University</h3>
+              <!-- <span class="card__status">1 hour ago</span> -->
+            </div>
+          </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a href="" class="card">
+        <img src="/ideable/media/cam.png" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+              <path />
+            </svg>
+            <div class="card__header-text">
+              <h3 class="card__title">Cambridge University</h3>
+              <!-- <span class="card__status">3 hours ago</span> -->
+            </div>
+          </div>
+          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+          </p>
+        </div>
+      </a>
+    </li>
+  </ul>
 </div>
 <?php
 include ADMIN . '/includes/footer.php';
